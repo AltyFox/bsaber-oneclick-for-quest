5575f95f23fe5555c106c80c1d40b33c3799efdc
AltyFox <bartat404@gmail.com>
2023-11-10 20:24:11 -0600

    Update header to include beatsaver.com

--- src/meta.js
+++ src/meta.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         BeatSaver Quest OneClick Installer
 // @namespace    https://bsaber.com/
-// @version      1.0.6
+// @version      1.1.0
 // @description  Simple Userscript to allow the OneClick buttons to install to your Quest over ADB right from your browser. for bsaber.com
 // @author       Alteran
 // @match        https://bsaber.com/*
+// @match        https://beatsaver.com/*
 // @grant        none
 // @downloadURL  https://bsquest.xyz/bsaber_oneclick/script.user.js
 // @updateURL    https://bsquest.xyz/bsaber_oneclick/script.user.js
