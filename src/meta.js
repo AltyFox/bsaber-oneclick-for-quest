f79803799abd8ebfc86ab969de48c216c402b20e
AltyFox <bartat404@gmail.com>
2023-11-14 14:42:30 -0600

    Poof

--- src/meta.js
+++ src/meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Quest OneClick Installer
 // @namespace    https://bsaber.com/
-// @version      1.2
+// @version      1.3
 // @description  Simple Userscript to allow the OneClick buttons to install to your Quest over ADB right from your browser.
 // @author       Alteran
 // @match        https://bsaber.com/*
@@ -9,5 +9,6 @@
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // @downloadURL  https://bsquest.xyz/bsaber_oneclick/index.user.js
+// @updateURL    https://bsquest.xyz/bsaber_oneclick/index.user.js
 
 // ==/UserScript==
